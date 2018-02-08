f40bebf900d7cecc5ca9ecf5014e9c18484a2019
Felicien MANZI <35733583+gitmanfel@users.noreply.github.com>
2018-02-08 15:07:45 +0100

    Add files via upload

---
+++ cats.php
@@ -0,0 +1,8 @@
+<html>
+<head>...</head>
+<body><h1>PHP BASICS <?php echo $_GET['nom']; ?>!</h1></body>
+
+<p>PHP permet au Serveur Web de "réfléchir avant de parler" et en informatique, "réfléchir" c'est calculer, càd, manipuler des informations et les retourner au Client (le navigateur) dans n'importe quel format (très souvent: du HTML, du XML, du JSON, voir du texte simple, du CSS, du javascript...).</p>
+<a href="http://localhost/test/index.php?nom=Ludocic">previous page</a>
+
+</html>
---
+++ index.php
@@ -0,0 +1,6 @@
+<html>
+<head>...</head>
+<body><h1>Bonjour <?php echo $_GET['nom']; ?>!</h1></body>
+<img src="calin.gif" alt="calin">
+<a href="http://localhost/test/cats.php?nom=Ludocic">Next page</a>
+</html>
